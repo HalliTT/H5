4eae7888ce6a4f0a30f16eeb8d9ee3e0df574983
Haraldur Fredberg <Haraldur@live.dk>
2026-01-22 08:55:55 +0100

    Onion

---
+++ OnionArchitecture1/MyBank.WebApi.Mysql/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using MyBank.Application.Interfaces;
+using MyBank.Application.Services;
+using MyBank.Domain.Services.Interfaces;
+using MyBank.Domain.Services.Services;
+using MyBank.Infrastructure.Repositories;
+
+namespace MyBank.WebApi.Mysql
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            var connectionString = builder.Configuration.GetConnectionString("MyBankMySql")
+                ?? throw new InvalidOperationException("MySQL connection string not found.");
+
+            builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+            builder.Services.AddScoped<IAccountAppService, AccountAppService>();
+            builder.Services.AddScoped<TransferDomainService>();
+
+            builder.Services.AddSwaggerGen(c => {
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                c.IgnoreObsoleteActions();
+                c.IgnoreObsoleteProperties();
+                c.CustomSchemaIds(type => type.FullName);
+            });
+
+            var app = builder.Build();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
